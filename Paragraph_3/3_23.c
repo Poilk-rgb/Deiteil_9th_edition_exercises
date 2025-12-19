0f6c37cda10aa3a71d604e7b21e5f06b00bc7a61
Poilk-rgb <vitalik26tkachenko@gmail.com>
2025-12-19 22:15:23 +0200

    Add files via upload

---
+++ Paragraph_3/3_23.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+    int main(void){
+        int counter = 1;
+        int number = 0;
+        int largest = 0;
+
+            while(counter != 11){
+                printf("%s%d%s", "Enter ", counter, "the number to get the biggest: ");
+                scanf("%d", &number);
+                if(number > largest){
+                    largest = number;
+                }
+            counter++;
+            }
+        printf("%s%d", "The largest number is: " , largest);
+    }
---
+++ Paragraph_3/3_24.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+
+    int main(void){
+        int number = 1;
+        int constant = 1;
+        int count = 0;
+
+
+
+        int count2 = 1;
+        while (count2 != 10000) {
+        printf("%d%s\t", count2, "*N");// рядок 8-13 взагалі не пов'язані з основною програмою, просто виводять №1, №10 і тд
+        count2 *= 10;
+        }
+        printf("\n\n");
+
+
+
+            while(number != 10000){
+                printf("%d\t", number);
+                number *= 10;
+                count++;
+                    if(number == 1000){
+                            if(count < 6){
+                        printf("%d\n", number);
+                        number = 2;
+                            }
+                    }
+                    if(number == 2000){
+                        printf("%d\n", number);
+                        number = 3;
+                    }
+                    if(number == 3000){
+                        printf("%d\n", number);
+                        number = 4;
+                    }
+                    if(number == 4000){
+                        printf("%d\n", number);
+                        number = 5;
+                    }
+                    if(number == 5000){
+                        printf("%d\n", number);
+                        number = 6;
+                    }
+                    if(number == 6000){
+                        printf("%d\n", number);
+                        number = 7;
+                    }
+                    if(number == 7000){
+                        printf("%d\n", number);
+                        number = 8;
+                    }
+                    if(number == 8000){
+                        printf("%d\n", number);
+                        number = 9;
+                    }
+                    if(number == 9000){
+                        printf("%d\n", number);
+                        number = 10;
+                    }
+            }
+    printf("%d\n", number);
+    }
