39110e871917869a984e566926340e569d8f1890
Poilk-rgb <vitalik26tkachenko@gmail.com>
2026-02-04 16:41:01 +0200

    Add files via upload

---
+++ Chapter5/5_28.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <math.h>
+
+int amount_of_digits(int);
+int digits_reversed(int);
+
+int main(void){
+    int number = 7631;
+    printf("%d", digits_reversed(number));
+}
+
+int amount_of_digits(int number){
+    int count = 0;
+    for(; number > 0; count++){
+        number /= 10;
+    }
+    return count;
+}
+
+int digits_reversed(int number){
+    int reversed_number = 0;
+    int store = amount_of_digits(number);
+    for(;store > 0;){
+        reversed_number += (number % 10) * pow(10, --store);
+        number /= 10;
+    }
+    return reversed_number;
+}
